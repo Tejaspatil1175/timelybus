1a7868b07295e4bc399b911aa69b6d47ab042913
Tejaspatil1175 <tejaspatil1175@gmail.com>
2024-12-15 11:30:52 +0530

    Connected database and created User model

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
+const connectToDb = require('./db/db');
 
-
+connectToDb();
 const app = express();
 app.use(cors());
 app.get('/',(req,res)=>{
