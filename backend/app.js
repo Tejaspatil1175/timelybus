3f83b348f445dd21811ccb96e4ae919a255d70a1
Tejaspatil1175 <tejaspatil1175@gmail.com>
2024-12-15 13:46:53 +0530

    Added user registration router

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,16 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const connectToDb = require('./db/db');
-
+const userRoutes = require('./routes/user.routes');
 connectToDb();
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/',(req,res)=>{
       res.sent('Hellow world');
 });
+app.use('/users', userRoutes);
 
 module.export = app;
